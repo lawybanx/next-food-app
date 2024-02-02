4fd740177ac5ab414f2093ec69cd4cecb9aef691
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2024-02-02 23:09:37 +0100

    feat: bg-umage WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { StoreProvider } from '@/lib/StoreProvider';
 import { Toaster } from '@/components/ui/sonner';
+import AppBackground from '@/components/AppBackground';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <StoreProvider>
           <Navbar />
+          <AppBackground />
           <div className='pt-20'>{children}</div>
         </StoreProvider>
         <Toaster position='top-right' closeButton richColors />
