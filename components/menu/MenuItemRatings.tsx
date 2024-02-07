040fd9e0fe67933bd8f67ad697e23f18f4281250
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2024-02-07 23:27:28 +0100

    feat: refactor overlay

--- components/menu/MenuItemRatings.tsx
+++ components/menu/MenuItemRatings.tsx
@@ -4,7 +4,7 @@ interface MenuItemRatingsProps {
 
 export default function MenuItemRatings({ ratings }: MenuItemRatingsProps) {
   return (
-    <div className='absolute top-0 bottom-0 left-0 w-full h-[256px] bg-gradient-to-t from-black to-transparent bg-opacity-50 flex justify-center items-end text-white animate-accordion-up'>
+    <div className='absolute top-0 bottom-0 left-0 w-full h-[256px] bg-gradient-to-t from-black to-transparent bg-opacity-50 flex justify-center items-end text-white '>
       <p>Rating: {ratings}</p>
     </div>
   );
