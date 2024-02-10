33de77ed2ecb6a8d7437d78b890690d862eb2f36
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2024-02-10 23:01:08 +0100

    feat: order page WIP

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import DeliveryForm from '@/components/checkout/DeliveryForm';
+import React from 'react';
 
 export default function page() {
   return (
-    <div className='pt-16'>
-      <h1>Checkout</h1>
+    <div className='bg-accent h-screen'>
+      <div className='mx-auto w-full max-w-5xl pt-10'>
+        <h1>Checkout</h1>
+        <div className='flex gap-5 justify-between'>
+          <DeliveryForm />
+          <section>Right</section>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
